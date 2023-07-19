61e8489be773e2e7dd6f8221f999f346e076f0b7
steven <crowscoupetech@proton.me>
2023-07-19 14:03:49 -0500

    2 passing

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left + 40 < 400) {
+    dodger.style.left = `${left + 7}px`;
+    }
+}
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left - 1 > 0) {
+      dodger.style.left = `${left - 7}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+  });
+
+  
